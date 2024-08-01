578ae84a0a4dc6bec48cbb593c27516780e49070
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-01 20:17:31 +0300

    add js selector

---
+++ app.js
@@ -0,0 +1,7 @@
+const againBtn = document.querySelector(".again");
+const number = document.querySelector(".number");
+const myInput = document.querySelector(".guess");
+const checkBtn = document.querySelector(".check");
+const message = document.querySelector(".msg");
+const myScore = document.querySelector(".my-score");
+const topScore = document.querySelector(".top-score");
