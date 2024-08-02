72a0010e30d72935569d6e054f819b284a699ec4
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-02 16:32:16 +0300

    function created

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ const checkBtn = document.querySelector(".check");
 const message = document.querySelector(".msg");
 const myScore = document.querySelector(".my-score");
 const topScore = document.querySelector(".top-score");
+
+let score = 0;
+let tryCount = 10;
+
+const randomNum = Math.floor(Math.random() * 20);
+console.log(randomNum);
+
+checkBtn.addEventListener("click", playToGame);
+
+function playToGame() {
+  const myNumber = myInput.value;
+  if (myNumber == "") {
+    message.textContent = "Lütfen boş bırakmayınız!";
+  } else if (myNumber != randomNum) {
+    message.textContent = "Bilemediniz";
+  } else {
+    message.textContent = "Tebrikler tahmininiz doğru!";
+  }
+}
