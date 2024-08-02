7d9a1269d83e0ae0064077bee60d579c2c245019
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-08-02 17:26:49 +0300

    againBtn added

--- app.js
+++ app.js
@@ -10,10 +10,11 @@ const bodyScreen = document.querySelector("body");
 let score = 0;
 let tryCount = 10;
 
-const randomNum = Math.floor(Math.random() * 20);
-console.log(randomNum);
-
 checkBtn.addEventListener("click", playToGame);
+againBtn.addEventListener("click", againGame);
+
+let randomNum = Math.floor(Math.random() * 20);
+console.log(randomNum);
 
 function playToGame() {
   const myNumber = myInput.value;
@@ -23,9 +24,27 @@ function playToGame() {
     message.textContent = "Bilemediniz";
     tryCount--;
     myScore.textContent = tryCount;
+    if (tryCount == 0) {
+      message.textContent =
+        "Tahmin hakkınız doldu. Yeniden oynamak için Again butonuna basınız.";
+      bodyScreen.style.backgroundColor = "red";
+      checkBtn.disabled = true;
+    }
   } else {
     message.textContent = "Tebrikler tahmininiz doğru!";
     bodyScreen.style.backgroundColor = "green";
     checkBtn.disabled = true;
+    number.textContent = randomNum;
   }
+}
+
+function againGame() {
+  randomNum = Math.floor(Math.random() * 20);
+  console.log(randomNum);
+  bodyScreen.style.backgroundColor = "#407A8A";
+  tryCount = 10;
+  myScore.textContent = tryCount;
+  myInput.value = "";
+  checkBtn.disabled = false;
+  playToGame();
 }
